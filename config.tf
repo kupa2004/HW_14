000326b1aeb27837fc7a5d4659c04048a7c72bb7
kupa2004 <98742842+kupa2004@users.noreply.github.com>
2022-03-09 01:47:35 +0300

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  credentials = file("cred-gcp.json")
+  project = "solar-semiotics-343520"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-test14"
+  machine_type = "f1-micro"
+  zone         = "us-west1-b"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
