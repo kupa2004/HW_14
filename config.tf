f7b237a288d813bca2274951a6cbf74ab3b948f6
nekrasova <nekrasova@tecforce.ru>
2022-03-09 13:43:30 +0300

    nginx

--- config.tf
+++ config.tf
@@ -24,8 +24,6 @@ resource "google_compute_instance" "default" {
   machine_type = "f1-micro"
   zone         = "us-west1-b"
 
-  tags = ["foo", "bar"]
-
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
