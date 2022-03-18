542c90c5988bdfd5af77828f3bd4ec6da7fb1223
kupa2004 <98742842+kupa2004@users.noreply.github.com>
2022-03-18 11:04:08 +0300

    Update config.tf

--- config.tf
+++ config.tf
@@ -26,7 +26,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-2004-focal-v20220303a"
     }
   }
 
